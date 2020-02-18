672ff8e7422d46386606347e74fb1d6bdb9b4f32
christyram98 <venkat.ramaiah@eruvaka.com>
2020-02-18 12:55:37 +0530

    updated test cases

--- test.js
+++ test.js
@@ -1,9 +1,42 @@
-let ValidationChecking = require('./index')
-let testUndefinedVariable = undefined
-let testVariable = null
-let emptyObject = {}
+const test = require('ava')
+const ValidationChecking = require('./index')
 
-console.log(ValidationChecking.isUndefined(testUndefinedVariable)) // true
-console.log(ValidationChecking.isNull(testUndefinedVariable)) // true
-console.log(ValidationChecking.isUndefinedOrNull(testUndefinedVariable)) // true
-console.log(ValidationChecking.isObjectEmpty(emptyObject)) // true
+const testVariable = 'ram'
+const testUndefinedVariable = undefined
+const testNullVariable = null
+const emptyObject = {}
+const testObject = { a: 1 }
+const arrayWithNullValues = [ 1, 2, null, 4 ]
+const arrayWithFullNullValues = [ null, null ]
+const array = [1, 2, 3, 4]
+const emptyArray = []
+
+test('undefined checking', t => {
+  t.true(ValidationChecking.isUndefined(testUndefinedVariable))
+  t.false(ValidationChecking.isUndefined(testVariable))
+})
+
+test('null checking', t => {
+  t.true(ValidationChecking.isNull(testNullVariable))
+  t.false(ValidationChecking.isNull(testVariable))
+})
+
+test('undefined or null checking', t => {
+  t.true(ValidationChecking.isUndefinedOrNull(testNullVariable))
+  t.false(ValidationChecking.isUndefinedOrNull(testVariable))
+})
+
+test('object checking', t => {
+  t.true(ValidationChecking.isObjectEmpty(emptyObject))
+  t.true(ValidationChecking.isObjectUndefinedOrNullOrEmpty(testNullVariable))
+  t.false(ValidationChecking.isNull(testObject))
+})
+
+test('array checking', t => {
+  t.true(ValidationChecking.isAnyElementUndefinedOrNull(arrayWithNullValues))
+  t.true(ValidationChecking.isAllElementsUndefinedOrNull(arrayWithFullNullValues))
+  t.true(ValidationChecking.isAllElementsNonUndefinedAndNull(array))
+  t.false(ValidationChecking.isAllElementsNonUndefinedAndNull(arrayWithNullValues))
+  t.false(ValidationChecking.isArrayEmpty(array))
+  t.true(ValidationChecking.isArrayEmpty(emptyArray))
+})
